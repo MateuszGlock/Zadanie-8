c2dbfd8a9c213cd91cc4ca9179ec08dac8eda0f7
Mateusz Glock <mateusz.glock@gmail.com>
2024-04-28 22:06:55 +0200

    test2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,4 @@
-// Import biblioteki lodash.throttle
+/*// Import biblioteki lodash.throttle
 import throttle from "lodash/throttle";
 
 // Funkcja, która zapisuje dane w Local Storage
@@ -13,16 +13,16 @@ function saveFormDataToLocalStorage() {
 
   localStorage.setItem("feedback-form-state", JSON.stringify(formData));
 }
-
+*/
 // Znajdź formularz i nasłuchuj zdarzenia "input" na polach formularza
-const feedbackForm = document.querySelector(".feedback-form");
+const feedbackForm = document.querySelector('.feedback-form');
 feedbackForm.addEventListener(
-  "input",
+  'input',
   throttle(saveFormDataToLocalStorage, 500)
 );
 // Funkcja, która wczytuje dane z Local Storage i wypełnia nimi pola formularza
 function loadFormDataFromLocalStorage() {
-  const savedFormData = localStorage.getItem("feedback-form-state");
+  const savedFormData = localStorage.getItem('feedback-form-state');
   if (savedFormData) {
     const formData = JSON.parse(savedFormData);
     const emailInput = document.querySelector('input[name="email"]');
@@ -34,27 +34,27 @@ function loadFormDataFromLocalStorage() {
 }
 
 // Wywołaj funkcję wczytującą dane podczas ładowania strony
-window.addEventListener("load", loadFormDataFromLocalStorage);
+window.addEventListener('load', loadFormDataFromLocalStorage);
 // Funkcja obsługująca wysłanie formularza
 function handleSubmit(event) {
   event.preventDefault();
 
   // Wyczyść Local Storage
-  localStorage.removeItem("feedback-form-state");
+  localStorage.removeItem('feedback-form-state');
 
   // Wyczyść pola formularza
   const emailInput = document.querySelector('input[name="email"]');
   const messageTextarea = document.querySelector('textarea[name="message"]');
-  emailInput.value = "";
-  messageTextarea.value = "";
+  emailInput.value = '';
+  messageTextarea.value = '';
 
   // Wyloguj dane do konsoli
   const formData = {
     email: emailInput.value,
     message: messageTextarea.value,
   };
-  console.log("Wysłane dane:", formData);
+  console.log('Wysłane dane:', formData);
 }
 
 // Nasłuchuj zdarzenia "submit" na formularzu
-feedbackForm.addEventListener("submit", handleSubmit);
+feedbackForm.addEventListener('submit', handleSubmit);
